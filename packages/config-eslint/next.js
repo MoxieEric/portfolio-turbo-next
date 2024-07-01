a3a7be99c47146f0aeeb3c33d3dab662137eb0fc
Eric Nowels <ericnowels@gmail.com>
2024-07-01 15:08:08 -0600

    update eslint config

--- packages/config-eslint/next.js
+++ packages/config-eslint/next.js
@@ -1,6 +1,6 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path')
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json')
 
 /*
  * This is a custom ESLint configuration for use with
@@ -12,36 +12,63 @@ const project = resolve(process.cwd(), "tsconfig.json");
  */
 
 module.exports = {
-  extends: [
-    ...[
-      "@vercel/style-guide/eslint/node",
-      "@vercel/style-guide/eslint/typescript",
-      "@vercel/style-guide/eslint/browser",
-      "@vercel/style-guide/eslint/react",
-      "@vercel/style-guide/eslint/next",
-    ].map(require.resolve),
-    "turbo",
-  ],
-  parserOptions: {
-    project,
-  },
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
-      },
-      node: {
-        extensions: [".mjs", ".js", ".jsx", ".ts", ".tsx"],
-      },
-    },
-  },
-  ignorePatterns: ["node_modules/", "dist/"],
-  // add rules configurations here
-  rules: {
-    "import/no-default-export": "off",
-  },
-};
+	extends: [
+		...[
+			'@vercel/style-guide/eslint/node',
+			'@vercel/style-guide/eslint/typescript',
+			'@vercel/style-guide/eslint/browser',
+			'@vercel/style-guide/eslint/react',
+			'@vercel/style-guide/eslint/next',
+		].map(require.resolve),
+		'turbo',
+	],
+	parserOptions: {
+		project,
+	},
+	globals: {
+		React: true,
+		JSX: true,
+	},
+	settings: {
+		'import/resolver': {
+			typescript: {
+				project,
+			},
+			node: {
+				extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx'],
+			},
+		},
+	},
+	ignorePatterns: ['node_modules/', 'dist/'],
+	// add rules configurations here
+	rules: {
+		'import/no-default-export': 'off',
+		'react/function-component-definition': [
+			'error',
+			{
+				namedComponents: 'arrow-function',
+				unnamedComponents: 'function-expression',
+			},
+		],
+		'sort-imports': [
+			'warn',
+			{
+				ignoreCase: false,
+				ignoreDeclarationSort: false,
+				ignoreMemberSort: false,
+				memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+				allowSeparatedGroups: true,
+			},
+		],
+		'unicorn/filename-case': [
+			'warn',
+			{
+				cases: {
+					kebabCase: true,
+					camelCase: true,
+					pascalCase: true,
+				},
+			},
+		],
+	},
+}
