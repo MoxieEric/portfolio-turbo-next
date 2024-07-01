f071897364e468ea8cf46c2b0940afad1c0a93d2
Eric Nowels <ericnowels@gmail.com>
2024-07-01 14:32:04 -0600

    placeholder content for web app

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  transpilePackages: ["@repo/ui"],
-};
+	reactStrictMode: true,
+	transpilePackages: ['@repo/ui'],
+}
--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -1,30 +1,32 @@
-import { type ReactNode } from "react";
+import { type ReactNode } from 'react'
 
 export function Card({
-  title,
-  children,
-  href,
+	title,
+	children,
+	href,
 }: {
-  title: string;
-  children: ReactNode;
-  href: string;
+	title: string
+	children: ReactNode
+	href?: string
 }): JSX.Element {
-  return (
-    <a
-      className="ui-group ui-rounded-lg ui-border ui-border-transparent ui-px-5 ui-py-4 ui-transition-colors hover:ui-border-neutral-700 hover:ui-bg-neutral-800/30"
-      href={`${href}?utm_source=create-turbo&utm_medium=with-tailwind&utm_campaign=create-turbo"`}
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      <h2 className="ui-mb-3 ui-text-2xl ui-font-semibold">
-        {title}{" "}
-        <span className="ui-inline-block ui-transition-transform group-hover:ui-translate-x-1 motion-reduce:ui-transform-none">
-          -&gt;
-        </span>
-      </h2>
-      <p className="ui-m-0 ui-max-w-[30ch] ui-text-sm ui-opacity-50">
-        {children}
-      </p>
-    </a>
-  );
+	return (
+		<div className='ui-group ui-rounded-lg ui-px-5 ui-py-4 ui-transition-colors ui-relative'>
+			<h2 className='ui-mb-3 ui-text-2xl ui-font-semibold'>{title}</h2>
+			<p className='ui-m-0 ui-max-w-[30ch] ui-text-sm ui-opacity-80 ui-group-hover:opacity-100'>
+				{children}
+			</p>
+			{href ? (
+				<a
+					className='ui-absolute ui-w-full ui-top-0 ui-bottom-0 ui-left-0 ui-right-0'
+					href={href}
+					rel='noopener noreferrer'
+					target='_blank'
+				>
+					<span className='ui-sr-only'>{title}</span>
+				</a>
+			) : (
+				''
+			)}
+		</div>
+	)
 }
