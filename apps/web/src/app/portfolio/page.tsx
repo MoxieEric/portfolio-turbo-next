40b94927418d77faee94651d8cc4dba3548d2005
Eric Nowels <ericnowels@gmail.com>
2024-07-01 19:47:24 -0600

    routes

---
+++ apps/web/src/app/portfolio/page.tsx
@@ -0,0 +1,12 @@
+import { HStack, Text, VStack } from '@chakra-ui/react'
+import { Card } from '@repo/ui/components'
+import Image from 'next/image'
+
+const Page: React.FC = () => {
+	return (
+		<VStack py={{ base: 8, md: 12 }} minH={96} justifyContent='center'>
+			<Text>Portfolio...</Text>
+		</VStack>
+	)
+}
+export default Page
---
+++ apps/web/src/app/resume/page.tsx
@@ -0,0 +1,6 @@
+import { Text } from '@chakra-ui/react'
+
+const Page: React.FC = () => {
+	return <Text>Check LinkedIn for my resume for now.</Text>
+}
+export default Page
