b20c1a1b3e1080ae3d34f17211f006c1f7c8f0f3
Eric Nowels <ericnowels@gmail.com>
2024-07-10 16:22:51 -0600

    stub types package

--- packages/ui/src/components/cards/PortfolioCard.tsx
+++ packages/ui/src/components/cards/PortfolioCard.tsx
@@ -1,48 +1,79 @@
 'use client'
 
-import { HStack, Heading, Image, Text } from '@chakra-ui/react'
+import {
+	Box,
+	Flex,
+	HStack,
+	Heading,
+	Image,
+	Text,
+	VStack,
+} from '@chakra-ui/react'
+import type { PortfolioItem } from '@repo/types/dist'
 import type { ReactNode } from 'react'
 import React from 'react'
 import { Pill } from '../molecules'
-import { FeatureCard } from './FeatureCard'
 
 interface PortfolioCardProps {
 	children?: ReactNode
-	title: string
-	// feature: {}
+	content: PortfolioItem
 }
 
-const PortfolioCard: React.FC<PortfolioCardProps> = ({ children, title }) => {
+const PortfolioCard: React.FC<PortfolioCardProps> = ({ children, content }) => {
 	return (
-		<FeatureCard
-			image={
+		<Flex
+			_dark={{
+				bg: 'gray.700',
+			}}
+			bg='gray.50'
+			flexDirection={{ base: 'column', md: 'row' }}
+			gap={{ base: 6, md: 4 }}
+			p={{ base: 4, md: 6 }}
+			rounded='xl'
+			w='full'
+		>
+			<Box
+				minW={48}
+				overflow='hidden'
+				rounded='xl'
+				shadow='md'
+				w={{ base: 'full', md: 48 }}
+			>
 				<Image
 					alt='BLX'
 					aspectRatio='1/1'
 					bg='gray.100'
 					loading='lazy'
-					maxW={48}
+					maxW={{ base: 'full' }}
 					objectFit='cover'
 					overflow='hidden'
 					rounded='lg'
 					shadow='md'
-					src='/portfolio/brewlogix-app.jpg'
+					src={content.featureImage}
+					w='full'
 				/>
-			}
-			title={title}
-			w='full'
-		>
-			<Text>Brief description of the project or app</Text>
-			<Heading size='sm'>2024</Heading>
-			<Text size='sm'>Design/UX | Front-end | DevOps</Text>
-			<HStack>
-				<Pill size='sm'> Typescript </Pill>
-				<Pill size='sm'> Typescript </Pill>
-				<Pill size='sm'> Typescript </Pill>
-				<Pill size='sm'> Next</Pill>
-			</HStack>
-			{children}
-		</FeatureCard>
+			</Box>
+			<Flex
+				flexDirection='column'
+				gap={{ base: 6, md: 2 }}
+				justifyContent='space-between'
+				px={4}
+			>
+				<VStack alignItems='start' gap={2} justifyContent='center'>
+					<Heading size='lg'>{content.title}</Heading>
+					<Text fontSize='lg'>{content.description}</Text>
+				</VStack>
+
+				<HStack>
+					{content.stack?.map((skill) => (
+						<Pill key={skill} size='sm'>
+							{skill}
+						</Pill>
+					))}
+				</HStack>
+				{children}
+			</Flex>
+		</Flex>
 	)
 }
 
